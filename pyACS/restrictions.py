a640b9bd29f3b23f306747ceca3ec8bc5801e5d6
schmidtc <schmidtc@gmail.com>
2013-08-17 16:09:22 -0500

    adding restrictions

--- pyACS/restrictions.py
+++ pyACS/restrictions.py
@@ -3,7 +3,16 @@ from config import PYACS_DATAPATH
 import os
 import pysal
 
-db = pysal.open(os.path.join(PYACS_DATAPATH,'table_restrictions.csv'),'r')
+pth = os.path.join(PYACS_DATAPATH,'table_restrictions.csv')
+
+#### Self Install...
+if not os.path.exists(pth):
+    from _restrictions import txt
+    with open(pth,'w') as o:
+        o.write(txt)
+#### ... End Self Install
+
+db = pysal.open(pth, 'r')
 header = db[0][0]
 tid = db[1:,0]
 nonblks = db[1:,1]
@@ -11,6 +20,3 @@ blks = db[1:,2]
 
 aval4blks = dict([x for x in zip(tid,blks) if x[1]])
 avalnonblks = dict([x for x in zip(tid,nonblks) if x[1]])
-
-
-
