42ae91a5a360adbbe5e86fe9586aa3182d86ffe4
boris dev <boris.dev2@gmail.com>
2013-08-19 19:27:36 -0700

    fetch just one shapefile and code lookup methods

--- pyACS/download_shps.py
+++ pyACS/download_shps.py
@@ -6,6 +6,7 @@ import os
 import pysal
 import urllib
 import config
+import glob
 DEBUG = config.DEBUG
 
 
@@ -21,6 +22,73 @@ DEBUG = config.DEBUG
 #        20m = 1:20,000,000
 #base_url = "http://www2.census.gov/geo/tiger/TIGER2010/BG/2010/"
 #name_template = "tl_2010_%s_bg10.zip" #2digit FIPS
+
+def CODE_LOOKUP():
+    """ 
+    Conveniently avoid manually looking-up a census fips and summary level codes
+
+        >>> code("tx") 
+        >>> code("TEXAS")
+        >>> code("north carolina")
+        >>> code("block")
+        >>> code("tract")
+        >>> code("block group")
+    """
+    # Source: http://factfinder2.census.gov/help/en/glossary/s/summary_level_code_list.htm
+    GEO_CODES = {}
+    GEO_CODES['County'] = '050'
+    GEO_CODES['Tract'] = '140'
+    GEO_CODES['Block Group'] = '150'
+    GEO_CODES['Block'] = '750'
+    GEO_CODES['Place'] = '160'
+    GEO_CODES['City'] = '170'
+    GEO_CODES['State-Congressional'] = '500'
+    GEO_CODES['State-Legislative--Upper'] = '610'
+    GEO_CODES['State-Legislative-Lower'] = '620'
+    GEO_CODES['zipcode'] = '860'
+    GEO_CODES['Elementary'] = '950'
+    GEO_CODES['Secondary'] = '960'
+    GEO_CODES['Unified'] = '970'
+
+    state2code={}
+    stateab2code=zip(config.STATE_AB,config.STATE_FIPS)
+    statename2code=zip([name.lower()for name in config.STATE_NAME],config.STATE_FIPS)
+    state2code.update(dict(stateab2code))
+    state2code.update(dict(statename2code))
+    code=state2code
+    code.update(dict(GEO_CODES))
+    return code
+
+CODE_LOOKUP=CODE_LOOKUP()
+
+CENSUS_URL = "http://www2.census.gov/geo/tiger/GENZ2010/"
+SHAPEFILE_TEMPLATE = "gz_2010_{state}_{geo}_00_500k.zip" #2digit FIPS, 3 digit Summary Level
+
+def fetch_shapefile(state,geolevel):
+    """
+        >>> fetch_shapefile("texas","Block Group")
+        saves these file to the current dir:
+            texas_Block_Group.shp
+            texas_Block_Group.dbf
+            texas_Block_Group.shx
+            texas_Block_Group.prj
+    """    
+    fname=SHAPEFILE_TEMPLATE.format(state=CODE_LOOKUP[state],geo=CODE_LOOKUP[geolevel])
+    print fname
+    url = urllib.urlopen(CENSUS_URL+fname)
+    dat = url.read()
+    with open(fname,'wb') as o:
+        o.write(dat)
+    os.system('unzip '+fname)
+    fname.replace('.zip','.shp')
+    fname.replace('.zip','.dbf')
+    # rename to a easier to understand filename:
+    prefix=fname.split(".")[0]
+    for filename in glob.glob(prefix+'*.*'):
+        suffix=filename.split(".")[1]
+        print filename
+        os.rename(filename,state+"_"+geolevel.replace(" ","_")+"."+suffix)
+
 base_url = "http://www2.census.gov/geo/tiger/TIGER2011/TRACT/"
 name_template = "tl_2011_%s_tract.zip" #2digit FIPS
 
@@ -54,4 +122,5 @@ def dl_merge(outname="tracts",sumlevel="140"):
     outshp.close()
     outdbf.close()
 if __name__=='__main__':
-    dl_merge(outname='/pyacs/tracts11',sumlevel='140')
+    #dl_merge(outname='/pyacs/tracts11',sumlevel='140')
+    fetch_shapefile("texas","Block Group")
