4d52237122c1630f443067ed35817fdf98c9f0d6
boris dev <boris.dev2@gmail.com>
2013-08-25 20:22:53 -0700

    LQs have for black race

--- race-map.py
+++ race-map.py
@@ -12,43 +12,103 @@
 
 TODO: 
 - sava data as a pytable
+- handle nan better for map classification
+- do it by state
 
 """
 import numpy
 import pysal
 import pyACS
 
-# get a list of census tract ids from the shapefile
-dbf = pysal.open('/pyacs/tracts11.dbf') 
-order = dbf.by_col('GEOID') # get the geoids, in same order as .shp
-t = pyACS.ACS()
 
-# get arrays of total pop and black pop values in the same order as the census tract ids
-total = t.get_ordered(order, 'B01003001')
-total = numpy.array(total) 
-black = t.get_ordered(order, 'B02009001')
-black = numpy.array(black) 
+### IN THE FUTURE I WILL REPLACE THIS WITH pyTABLES
 
-# replace null values with 0, so no division by zero
-print len(total[total==0]), "zero values"
-total[total==0] = np.nan # convert zeros to nan 
-print numpy.sum(numpy.isnan(total)), "nan values"
+# Extract tract ids from the shapefile
+dbf = pysal.open('/pyacs/tracts11.dbf')
+geoids = dbf.by_col('GEOID') # get the geoids, in same geoids as .shp
+# Question: why is GEOID != STATE + COUNTY + TRACT ?
+
+# The Magic
+datasetObj = pyACS.ACS()
+# arrays of total pop and black pop values ordered by shapefile's tract ids
+tract_total = numpy.array(datasetObj.get_ordered(geoids, 'B01003001'))
+tract_black = numpy.array(datasetObj.get_ordered(geoids, 'B02009001'))
+N=len(tract_black)
+
+# demoninator: parent state's black population / total population
+parent = numpy.array([ i[:2] for i in geoids ]) # state parent of each tract
+states=list(set(parent))
+print len(states), "unique state ids"
+
+parent_state_total=numpy.zeros(N)
+parent_state_black=numpy.zeros(N)
+
+for state in states:
+    parent_state_total[parent==state] = tract_total[parent==state].sum()
+    parent_state_black[parent==state] = tract_black[parent==state].sum()
+
+
+# replace 0 with null values, so no division by zero
+#Nzeros=len(myarray[myarray==0]), "count zero values"
+#print Nzeros, "count zero values"
+#if Nzeros >0:
+#    myarray[myarray==0] = numpy.nan # convert zeros to nan 
+#    Nnulls=numpy.sum(numpy.isnan(myarray)), "count nan values"
+#    if Nzeros==Nnulls:
+#        return myarray
+#    else:
+#        print "ERROR: bug in function"
+
+
 
 # calculate location quotients
-grandtotal=total.sum() # 310,346,358
-blacktotal=black.sum() # 41,917,507
-blackportionusa=blacktotal/grandtotal # 0.135
-lqs=black/total/blackportionusa
+numerator=tract_black/tract_total
+denomenator=parent_state_black/parent_state_total
+
+LQs = numerator/denomenator
+print numpy.isnan(lqs).sum() , "count nan in LQs"
+# remove 689 tracts with (zero pop) nans
+# so we can use pysal's classification code
+LQs = LQs[numpy.logical_not(numpy.isnan(LQs))]
+
 
 from pysal.esda.mapclassify import *
 # source: http://pysal.org/1.3/_modules/pysal/esda/mapclassify.html
 #fj=Fisher_Jenks(lqs) # takes too long for real-time map rendering
-p=Percentiles(lqs, pct=[1,10,40,60,90,99,100])
+p=Percentiles(LQs, pct=[1,10,40,60,90,99,100])
 print p.bins
 print p.counts
 print p.k
 
+
+"""
+http://colorbrewer2.org/ PRgn
+BIN COUNT COLOR COLORLABEL
+0-1% 4840       (27, 120, 55)       GREEN
+1-10% 2492      (127, 191, 123)
+10-40% 21993    (217, 240, 211)
+40-60% 14662    (247, 247, 247)
+60-90% 21993    (231, 212, 232)
+90-99% 6598     (175, 141, 195)
+99-100 734      (118, 42, 131)      PURPLE
+
+(27, 120, 55)     
+(127, 191, 123)
+(217, 240, 211)
+(247, 247, 247)
+(231, 212, 232)
+(175, 141, 195)
+(118, 42, 131)    
+
+"""
+
+
 def make_lqs():
+    ## The old way was to use national level demoninator instead of statelevel:
+    #grandtotal=total.sum() # 310,346,358
+    #blacktotal=black.sum() # 41,917,507
+    #blackportionusa=blacktotal/grandtotal # 0.135
+
     """
     Examples
     --------
@@ -78,7 +138,6 @@ def make_lqs():
     >>> p2.k
     """
     pass
-
 
 
 def _test():
