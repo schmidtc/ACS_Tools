78db95e0999476b115723e847f6ccea87a8b3e22
boris dev <boris.dev2@gmail.com>
2013-08-27 21:03:41 -0700

    ints to floats to nan, also LQs calculated using pyTables as input

--- race-map.py
+++ race-map.py
@@ -22,21 +22,13 @@ import pyACS
 import tables
 
 
-### IN THE FUTURE I WILL REPLACE THIS WITH pyTABLES
 f=tables.open_file("census_variables.h5")
+print "pyTable Fields Description: ", f.root.tracts.readout
 tract_total=f.root.tracts.readout.cols.tractcount[:] 
 tract_black=f.root.tracts.readout.cols.blackcount[:] 
-
-# Extract tract ids from the shapefile
-#dbf = pysal.open('/pyacs/tracts11.dbf')
-#geoids = dbf.by_col('GEOID') # 11chars = st2+county3+tract6
-#datasetObj = pyACS.ACS()
-#tract_total = numpy.array(datasetObj.get_ordered(geoids, 'B01003001'))
-#tract_black = numpy.array(datasetObj.get_ordered(geoids, 'B02009001'))
+geoids=f.root.tracts.readout.cols.geoid[:] 
 
 N=len(tract_black)
-
-# demoninator: parent state's black population / total population
 parent = numpy.array([ i[:2] for i in geoids ]) # state parent of each tract
 states=list(set(parent))
 print len(states), "unique state ids"
@@ -48,17 +40,17 @@ for state in states:
     parent_state_total[parent==state] = tract_total[parent==state].sum()
     parent_state_black[parent==state] = tract_black[parent==state].sum()
 
+# array needs to be a float to use nan
+tract_black = tract_black.astype(numpy.float)
+tract_total = tract_total.astype(numpy.float)
 
 # replace 0 with null values, so no division by zero
-#Nzeros=len(myarray[myarray==0]), "count zero values"
-#print Nzeros, "count zero values"
-#if Nzeros >0:
-#    myarray[myarray==0] = numpy.nan # convert zeros to nan 
-#    Nnulls=numpy.sum(numpy.isnan(myarray)), "count nan values"
-#    if Nzeros==Nnulls:
-#        return myarray
-#    else:
-#        print "ERROR: bug in function"
+Nzeros=len(tract_total[tract_total==0])
+print Nzeros, "count zero values"
+if Nzeros >0:
+    tract_total[tract_total==0] = numpy.nan # convert zeros to nan 
+    Nnulls=numpy.sum(numpy.isnan(tract_total)), "count nan values"
+    if Nzeros==Nnulls: print "OK now"
 
 
 
@@ -67,7 +59,7 @@ numerator=tract_black/tract_total
 denomenator=parent_state_black/parent_state_total
 
 LQs = numerator/denomenator
-print numpy.isnan(lqs).sum() , "count nan in LQs"
+print numpy.isnan(LQs).sum() , "count nan in LQs"
 # remove 689 tracts with (zero pop) nans
 # so we can use pysal's classification code
 LQs = LQs[numpy.logical_not(numpy.isnan(LQs))]
@@ -93,14 +85,6 @@ BIN COUNT COLOR COLORLABEL
 90-99% 6598     (175, 141, 195)
 99-100 734      (118, 42, 131)      PURPLE
 
-(27, 120, 55)     
-(127, 191, 123)
-(217, 240, 211)
-(247, 247, 247)
-(231, 212, 232)
-(175, 141, 195)
-(118, 42, 131)    
-
 Each color should be in HEX RGB format beginning with a '#'.
 The first color in the list provided is assigned to class 2, the next to class 3 and so on.
 
@@ -111,46 +95,10 @@ Since this color will be made transparent.
 
 
 def make_lqs():
-    ## The old way was to use national level demoninator instead of statelevel:
-    #grandtotal=total.sum() # 310,346,358
-    #blacktotal=black.sum() # 41,917,507
-    #blackportionusa=blacktotal/grandtotal # 0.135
-
-    """
-    Examples
-    --------
-
-    >>> cal=load_example()
-    >>> fj=Fisher_Jenks(cal)
-    >>> fj.adcm
-    832.8900000000001
-    >>> fj.bins
-    [110.73999999999999, 192.05000000000001, 370.5, 722.85000000000002, 4111.4499999999998]
-    >>> fj.counts
-    array([50,  2,  4,  1,  1])
-
-
-    ### PERCENTILES
-    >>> p=Percentiles(dataarray, pct=[1,10,50,90,99,100])
-    >>> p.bins
-    array([  1.35700000e-01,   5.53000000e-01,   9.36500000e+00,
-             2.13914000e+02,   2.17994800e+03,   4.11145000e+03])
-    >>> p.counts
-    array([ 1,  5, 23, 23,  5,  1])
-    >>> p2=Percentiles(cal,pct=[50,100])
-    >>> p2.bins
-    array([    9.365,  4111.45 ])
-    >>> p2.counts
-    array([29, 29])
-    >>> p2.k
-    """
     pass
 
 
-def _test():
-    import doctest
-    doctest.testmod(verbose=True)
 
 if __name__ == '__main__':
-    _test()
+    pass
 
