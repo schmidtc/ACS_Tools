5434744328209e709247c31705ae4f661b79cbca
borisdev <boris.dev@gmail.com>
2013-09-01 03:31:06 +0000

    fixed fetch block shapefiles

--- pyACS/download_shps.py
+++ pyACS/download_shps.py
@@ -165,15 +165,15 @@ def fetch_and_merge_state_block_shapefiles(outname="blocks"):
 
 def fetch_blockshapefile(state):
     CENSUS_URL = "http://www2.census.gov/geo/tiger/TIGER2010/TABBLOCK/2010/"
-    SHAPEFILE_TEMPLATE = "tl_2010_%s_tabblock10.zip" #2digit STATE FIPS
-
-    fname=SHAPEFILE_TEMPLATE.format(state=CODE_LOOKUP[state])
-    print fname
-    url = urllib.urlopen(CENSUS_URL+fname)
+    SHAPEFILE_TEMPLATE = "tl_2010_{statecode}_tabblock10.zip" #2digit STATE FIPS
+    fname=SHAPEFILE_TEMPLATE.format(statecode=CODE_LOOKUP[state])
+    URL=CENSUS_URL+fname
+    print URL
+    url = urllib.urlopen(URL)
     dat = url.read()
     with open("/vol/"+fname,'wb') as o:
         o.write(dat)
-    r=os.system('unzip '+fname)
+    r=os.system('unzip '+"/vol/"+fname)
     print "unzip result", r
     fname.replace('.zip','.shp')
     fname.replace('.zip','.dbf')
@@ -183,4 +183,6 @@ if __name__=='__main__':
     #dl_merge(outname='/pyacs/tracts11',sumlevel='140')
     # fetch_shapefile("texas","Block Group")
     #fetch_and_merge_state_block_shapefiles(outname="blocks") 
-    fetch_blockshapefile("delaware")
+    for st in config.STATE_NAME: 
+        st = st.lower()
+        fetch_blockshapefile(st)
